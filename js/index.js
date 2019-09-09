4d9cc00854722a5faf98c874853aa9c639d57d17
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-09 11:24:53 -0700

    Zoom In and Zoom out added for Navgiation using mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+// Your code goes hereimg-content
+
+const navZoomInAndOut = document.querySelectorAll('nav a');
+navZoomInAndOut.forEach((navigation) => {
+    navigation.addEventListener('mouseover', (event) => {
+      navigation.style.transform = 'scale(1.6)';
+      navigation.style.transition = 'transform 0.5s';
+      navigation.style.color = 'blue';
+    });
+    navigation.addEventListener('mouseout', (event) => {
+        navigation.style.transform = 'scale(1.0)';
+        navigation.style.transition = 'transform 0.5s';
+        navigation.style.color = 'black';
+     });
+});
+
