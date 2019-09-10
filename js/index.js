f3825dee7c22839b7a361514ab628d75184458a4
Shafi Masoumi <masoumi2017@icloud.com>
2019-09-09 17:32:13 -0700

    click & dblclick updated

--- js/index.js
+++ js/index.js
@@ -3,14 +3,29 @@
 const navZoomInAndOut = document.querySelectorAll('nav a');
 navZoomInAndOut.forEach((navigation) => {
     navigation.addEventListener('mouseover', (event) => {
-      navigation.style.transform = 'scale(1.6)';
-      navigation.style.transition = 'transform 0.5s';
-      navigation.style.color = 'blue';
+        navigation.style.transform = 'scale(1.6)';
+        navigation.style.transition = 'transform 0.5s';
+        navigation.style.color = 'blue';
     });
     navigation.addEventListener('mouseout', (event) => {
         navigation.style.transform = 'scale(1.0)';
         navigation.style.transition = 'transform 0.5s';
-        navigation.style.color = 'black';
-     });
+        navigation.style.color = '';
+    });
 });
+
+const button = document.querySelectorAll('.btn');
+button.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.target.style.background = 'seagreen';
+        event.target.style.color = 'yellow';
+    })
+    element.addEventListener('dblclick', (event) => {
+        event.target.style.background = '';
+        event.target.style.color = '';
+    })
+
+});
+
+
 
